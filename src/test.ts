c9fcf6be26aa1a55a383c396aa6d93d4c2225527
PECKOUET-BINOMBO <apeckouet@gmail.com>
2025-08-05 15:26:29 +0000

    test conosol en dur

--- src/test.ts
+++ src/test.ts
@@ -5,31 +5,48 @@ import { Alimentaire } from "./Model/Alimentaire";
 import { Chimique } from "./Model/Chimique";
 import { Fragile } from "./Model/Fragile";
 import { Incassable } from "./Model/Incassable";
-
-// Création des cargaisons
-const cargaisonAerienne = new Aerienne(500);
-const cargaisonMaritime = new Maritime(800);
-const cargaisonRoutiere = new Routiere(300);
+import { Cargaison } from "./Model/Cargaison";
+import { Produit } from "./Model/Produit";
 
 // Création des produits
-const tomates = new Alimentaire("Tomates", 50);
-const acide = new Chimique("Acide sulfurique", 100, 8);
-const verre = new Fragile("Verre", 30);
-const fer = new Incassable("Fer", 200);
+const produits: Produit[] = [
+    new Alimentaire("Tomates", 50),
+    new Chimique("Acide sulfurique", 100, 8),
+    new Fragile("Verre", 30),
+    new Incassable("Fer", 200)
+];
 
-// Ajout des produits
-cargaisonAerienne.ajouterProduit(tomates);
-cargaisonMaritime.ajouterProduit(acide);
-cargaisonRoutiere.ajouterProduit(verre);
-cargaisonRoutiere.ajouterProduit(fer);
+// Création des cargaisons
+const cargaisons: Cargaison[] = [
+    new Aerienne(500),
+    new Maritime(800),
+    new Routiere(300)
+];
 
+// Ajout des produits aux cargaisons
+cargaisons[0].ajouterProduit(produits[0]); // Tomates dans Aerienne
+cargaisons[1].ajouterProduit(produits[1]); // Acide dans Maritime
+cargaisons[2].ajouterProduit(produits[2]); // Verre dans Routiere
+cargaisons[2].ajouterProduit(produits[3]); // Fer dans Routiere
 
-// Affichage détaillé des cargaisons
-const cargaisons = [cargaisonAerienne, cargaisonMaritime, cargaisonRoutiere];
+// Affichage de la liste des produits
+console.log("\nListe des produits :");
+produits.forEach((p, i) => {
+    console.log(`[${i}] ${p.info()}`);
+});
+
+// Affichage de la liste des cargaisons et leurs infos
 cargaisons.forEach((c, i) => {
     console.log(`\nCargaison #${i} (${c.constructor.name})`);
-    console.log(`Distance: ${c.getDistance()} km`);
-    console.log(`Produits: ${c.nbProduit()}`);
-    c.getProduits().forEach(p => console.log(" - " + p.info())); // Affiche le nom et les infos
-    console.log(`Total frais: ${c.sommeTotale()} F`);
+    console.log(`  Distance : ${c.getDistance()} km`);
+    console.log(`  Nombre de produits : ${c.nbProduit()}`);
+    console.log(`  Produits :`);
+    if (c.getProduits().length === 0) {
+        console.log("    Aucun produit.");
+    } else {
+        c.getProduits().forEach((p, j) => {
+            console.log(`    [${j}] ${p.info()}`);
+        });
+    }
+    console.log(`  Total frais : ${c.sommeTotale()} F`);
 });
